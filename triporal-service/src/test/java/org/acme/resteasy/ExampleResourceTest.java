a2c64bed9f85b6f66ad072301f1acade1e97c2d7
Eric Murphy <murphye@gmail.com>
2020-10-19 00:06:49 -0400

    Add initial saga code

--- triporal-service/src/test/java/org/acme/resteasy/ExampleResourceTest.java
+++ triporal-service/src/test/java/org/acme/resteasy/ExampleResourceTest.java
@@ -12,10 +12,9 @@ public class ExampleResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/resteasy/hello")
+          .when().get("/saga")
           .then()
-             .statusCode(200)
-             .body(is("hello"));
+             .statusCode(200);
     }
 
 }
