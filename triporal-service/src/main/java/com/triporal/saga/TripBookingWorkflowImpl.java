b8e22eb86711536ac2d8ae9abd61cccf23efb7b9
Eric Murphy <murphye@gmail.com>
2021-01-13 18:21:55 -0500

    Update TripBookingWorkflowImpl.java

--- triporal-service/src/main/java/com/triporal/saga/TripBookingWorkflowImpl.java
+++ triporal-service/src/main/java/com/triporal/saga/TripBookingWorkflowImpl.java
@@ -20,11 +20,17 @@
 package com.triporal.saga;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.operators.multi.processors.UnicastProcessor;
 import io.temporal.activity.ActivityOptions;
 import io.temporal.common.RetryOptions;
 import io.temporal.failure.ActivityFailure;
+import io.temporal.workflow.Async;
+import io.temporal.workflow.Promise;
 import io.temporal.workflow.Saga;
 import io.temporal.workflow.Workflow;
+import io.vertx.core.json.JsonObject;
+
 import java.time.Duration;
 
 import com.triporal.model.HotelBooking;
@@ -42,12 +48,28 @@ public class TripBookingWorkflowImpl implements TripBookingWorkflow {
 
   @Override
   public HotelBooking bookTrip(String name) {
+
+    UnicastProcessor<String> processor = UnicastProcessor.create();
+    Multi<JsonObject> bookingStream = processor.onItem().transform(JsonObject::mapFrom);
+    
     // Configure SAGA to run compensation activities in parallel
     Saga.Options sagaOptions = new Saga.Options.Builder().setParallelCompensation(true).build();
     Saga saga = new Saga(sagaOptions);
+
     try {
-      String carReservationID = activities.reserveCar(name);
-      saga.addCompensation(activities::cancelCar, carReservationID, name);
+
+      Promise<String> carReservationIDPromise = Async.function(activities::reserveCar, name);
+
+      carReservationIDPromise.thenApply(carReservationID -> {
+        saga.addCompensation(activities::cancelCar, carReservationID, name);
+        processor.onNext(carReservationID);
+        return carReservationID;
+      });
+
+
+
+      //String carReservationID = activities.reserveCar(name);
+      //saga.addCompensation(activities::cancelCar, carReservationID, name);
 
       HotelBooking hotelBooking = activities.bookHotel(name);
       saga.addCompensation(activities::cancelHotel, hotelBooking.transactionId, name);
